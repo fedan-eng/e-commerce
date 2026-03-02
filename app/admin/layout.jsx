c81015357a2b1b2ee620d38e2df560c6bfea7aa2
excellar-BX <excellenceay33@gmail.com>
2026-03-02 02:03:41 +0100

    Fixed Responsiveness on nav

--- app/admin/layout.jsx
+++ app/admin/layout.jsx
@@ -88,9 +88,15 @@ export default function AdminLayout({ children }) {
           })}
         </nav>
 
-        <div style={{ padding: "16px 24px", borderTop: "1px solid #222", fontSize: "11px", color: "#444" }}>
-          {user?.firstName} {user?.lastName}
+        <div className="px-3 py-6" style={{ borderTop: "1px solid #222", fontSize: "11px", color: "#444" }}>
+          <div>
+            {user?.firstName} {user?.lastName}
           <div style={{ color: "#2a6e2a", marginTop: "2px" }}>● admin</div>
+          </div>
+          <Link  className="hover:bg-[#1e1e1e] bg-[#2a2a2a] py-3 px-5 rounded-lg" href="/" style={{ marginTop: "8px", color:"red", display: "flex", alignItems: "center", gap: "4px" }}>
+            <LogOut size={14} />
+            Logout
+          </Link>
         </div>
       </aside>
 
