e3e4383cdbecac9635f10056c7efdb0cd1f137a4
excellar-BX <excellenceay33@gmail.com>
2026-03-06 08:51:35 +0100

    product share and share modal

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -385,7 +385,7 @@ const shareText = `Check out ${product?.name} on FIL Store!`;
           
           <button
     onClick={() => setShowShareModal(true)}
-    className={`flex justify-end items-center ${product.originalPrice && 'md:absolute md:top-10 md:right-10' }  w-fit ${product.originalPrice ? "" : "justify-end"}  gap-1.5 px-3 py-1.5 border border-[#e5e5e5] rounded-full text-xs text-[#3e3e3e] hover:bg-[#f5f5f5] transition-colors cursor-pointer`}
+    className={`flex justify-end items-center ${product.originalPrice && 'md:absolute md:top-10 md:right-10' } max-[500px]:absolute max-[500px]:top-10 max-[500px]:right-10  w-fit ${product.originalPrice ? "" : "justify-end"}  gap-1.5 px-3 py-1.5 border border-[#e5e5e5] rounded-full text-xs text-[#3e3e3e] hover:bg-[#f5f5f5] transition-colors cursor-pointer`}
   >
     <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5"><circle cx="18" cy="5" r="3"/><circle cx="6" cy="12" r="3"/><circle cx="18" cy="19" r="3"/><line x1="8.59" y1="13.51" x2="15.42" y2="17.49"/><line x1="15.41" y1="6.51" x2="8.59" y2="10.49"/></svg>
     Share
