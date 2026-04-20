f490fcd0b82d87105ac49d065c224eb2c60ecc19
excellar-BX <excellenceay33@gmail.com>
2026-04-20 19:47:09 +0100

    fix verify payment page order details

--- app/api/verify-payment/route.js
+++ app/api/verify-payment/route.js
@@ -526,6 +526,7 @@ console.log("💾 No existing order found. Creating new order...");
             <th>Item</th>
             <th style="text-align: center;">Qty</th>
             <th style="text-align: right;">Price</th>
+            <th style="text-align: right;">Color</th>
             <th style="text-align: right;">Total</th>
           </tr>
         </thead>
@@ -535,8 +536,8 @@ console.log("💾 No existing order found. Creating new order...");
               <td>${item.name}</td>
               <td style="text-align: center;">${item.quantity}</td>
               <td style="text-align: right;">₦${Number(item.price).toLocaleString()}</td>
-              <td style="text-align: right;">₦${(Number(item.price) * Number(item.quantity)).toLocaleString()}</td>
               ${item.color?? (<td style="text-align: center;">Color: {item.color}</td> )}
+              <td style="text-align: right;">₦${(Number(item.price) * Number(item.quantity)).toLocaleString()}</td>
             </tr>
           `).join('')}
         </tbody>
