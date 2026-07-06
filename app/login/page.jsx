deddfa18099b3c586b7ea882076e1466c940a7a6
excellar-BX <excellenceay33@gmail.com>
2026-07-06 12:33:16 +0100

    nxjnxink

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import LoginForm from "@/components/LoginForm";
 
+export const dynamic = 'force-dynamic';
+
 const LoginPage = () => {
   return (
     <div className="">
